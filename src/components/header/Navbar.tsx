f051db7f598d34339ebd23d7ce874353e9ee98d9
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-20 23:05:09 +0800

    feat: add Tab Component

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,5 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+
 const Navbar = () => {
-  return <nav className="">Navbar</nav>;
+  return (
+    <nav className="absolute right-0 top-0">
+      <Tabs>
+        <TabsList>
+          <TabsTrigger value="Projects">Projects</TabsTrigger>
+          <TabsTrigger value="Resume">Resume</TabsTrigger>
+          <TabsTrigger value="Contact">Contact</TabsTrigger>
+        </TabsList>
+        <TabsContent value="Projects">Projects</TabsContent>
+        <TabsContent value="Resume">Resume</TabsContent>
+        <TabsContent value="Contact">Contact</TabsContent>
+      </Tabs>
+    </nav>
+  );
 };
 
 export default Navbar;
