aa88fccc78962473885d0e7c8cdb225ab4b04f45
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-20 22:50:28 +0800

    feat: created sidebar about component

---
+++ src/components/sidebar/About.tsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div className="mt-4 flex flex-col items-start">
+      <span className="text-start font-semibold">About</span>
+      <p className="text-sm">
+        A passionate developer who gained coding skills independently through
+        self-study and practice.
+      </p>
+    </div>
+  );
+};
+
+export default About;
