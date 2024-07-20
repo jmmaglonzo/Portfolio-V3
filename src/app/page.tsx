84cbc823f3a3eca0ce32e41c601e9657191aed88
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-20 22:49:55 +0800

    feat: Started the frontend for the main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import MainContent from "@/components/main/MainContent";
+import Sidebar from "@/components/sidebar/Sidebar";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="container m-16 mt-[100px] flex items-stretch justify-center gap-6">
+      <aside className="sticky top-16 z-10 h-full max-h-max rounded-md bg-slate-800 px-7 pb-8 pt-16">
+        <Sidebar />
+      </aside>
+      <MainContent />
+    </main>
+  );
 }
