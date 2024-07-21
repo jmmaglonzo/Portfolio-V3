0eb9c4268e1350da4f75e317a33648a76d7a289f
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 23:12:33 +0800

    feat: add Card Data

---
+++ src/utils/cards.ts
@@ -0,0 +1,35 @@
+import bradwell from "/public/bradwell.webp";
+import gymfit from "/public/gymfit.webp";
+import omnifood from "/public/omni-food.webp";
+import portfolio from "/public/portfoliov2.webp";
+import digibite from "/public/digibite-kitchen.webp";
+
+const cards = [
+  {
+    name: "Bradwell Sales & Marketing Corp.",
+    image: bradwell,
+    link: "https://bradwell-fe.vercel.app/",
+  },
+  {
+    name: "Digibite | Digital Menu",
+    image: digibite,
+    link: "https://digibite.vercel.app/",
+  },
+  {
+    name: "GymFit",
+    image: gymfit,
+    link: "https://gymfit-sage.vercel.app/",
+  },
+  {
+    name: "Omnifood-Ai",
+    image: omnifood,
+    link: "https://omnifood-react.vercel.app/",
+  },
+  {
+    name: "Portfolio v2",
+    image: portfolio,
+    link: "https://lonzo-react.vercel.app/",
+  },
+];
+
+export default cards;
