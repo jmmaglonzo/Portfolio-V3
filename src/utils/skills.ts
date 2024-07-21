43f10d1f62c34e5653985001b0d69eceb8de0d30
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 18:48:33 +0800

    feat: add skills array of object

---
+++ src/utils/skills.ts
@@ -0,0 +1,95 @@
+import html from "/public/icons/html.webp";
+import css from "/public/icons/css.webp";
+import js from "/public/icons/javascript.webp";
+import tw from "/public/icons/tailwind.webp";
+import bs from "/public/icons/bootstrap.webp";
+import sass from "/public/icons/sass.webp";
+import ts from "/public/icons/typescript.webp";
+import react from "/public/icons/react.webp";
+import next from "/public/icons/next.webp";
+import node from "/public/icons/node.webp";
+import express from "/public/icons/express.webp";
+import mongo from "/public/icons/mongo.webp";
+import git from "/public/icons/git.webp";
+import shadcn from "/public/icons/shadcn.webp";
+import reactform from "/public/icons/react-hook-form.webp";
+import zod from "/public/icons/zod.webp";
+import tanstack from "/public/icons/react-query.webp";
+import zustand from "/public/icons/zustand.webp";
+
+const icons = [
+  {
+    name: "Html",
+    icon: html,
+  },
+  {
+    name: "Css",
+    icon: css,
+  },
+  {
+    name: "Javascript",
+    icon: js,
+  },
+  {
+    name: "Tailwind",
+    icon: tw,
+  },
+  {
+    name: "Bootstrap",
+    icon: bs,
+  },
+  {
+    name: "Sass",
+    icon: sass,
+  },
+  {
+    name: "Typescript",
+    icon: ts,
+  },
+  {
+    name: "React",
+    icon: react,
+  },
+  {
+    name: "Next Js",
+    icon: next,
+  },
+  {
+    name: "Node Js",
+    icon: node,
+  },
+  {
+    name: "Express",
+    icon: express,
+  },
+  {
+    name: "Mongo DB",
+    icon: mongo,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+  {
+    name: "Shadcn",
+    icon: shadcn,
+  },
+  {
+    name: "React-Hook-Form",
+    icon: reactform,
+  },
+  {
+    name: "Zod",
+    icon: zod,
+  },
+  {
+    name: "Tanstack-Query",
+    icon: tanstack,
+  },
+  {
+    name: "Zustand",
+    icon: zustand,
+  },
+];
+
+export default icons;
