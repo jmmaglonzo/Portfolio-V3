4317ce99b80b3161e81b838fe5bbabb16651fe7a
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 23:11:51 +0800

    style: adjust padding and background color

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,14 @@
 import { Separator } from "../ui/separator";
 import Profile from "./Profile";
 import About from "./About";
-import Contact from "./Contact";
+import SidebarInfo from "./SidebarInfo";
 const Sidebar = () => {
   return (
-    <aside className="hidden h-full max-h-max rounded-md bg-slate-800 px-7 pb-8 pt-16 xl:sticky xl:top-16 xl:block">
+    <aside className="hidden h-full max-h-max rounded-md bg-secondary px-4 pb-8 pt-16 xl:sticky xl:top-16 xl:block">
       <Profile />
       <Separator decorative className="mt-4 h-[2px] rounded-sm" />
       <About />
-      <Contact />
+      <SidebarInfo />
     </aside>
   );
 };
