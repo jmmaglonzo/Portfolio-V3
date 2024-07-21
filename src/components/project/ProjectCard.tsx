972db87ef29dd14def7bf85024ef3b06bb9ec9cb
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 23:09:35 +0800

    feat: add card data

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -1,24 +1,33 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { AspectRatio } from "../ui/aspect-ratio";
 import { Card, CardContent, CardFooter } from "../ui/card";
 import Bradwell from "/public/bradwell.webp";
-const ProjectCard = () => {
+interface ProjectCardProps {
+  data: {
+    name: string;
+    image: StaticImageData;
+  };
+}
+
+const ProjectCard = ({ data: { name, image } }: ProjectCardProps) => {
   return (
     <Card className="border-none">
       <CardContent className="p-2">
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            src={Bradwell}
-            alt="Bradwell"
-            className="rounded-md object-cover"
-            fill
-            priority
-            sizes="(min-width: 1520px) 289px, (min-width: 1280px) calc(13.64vw + 84px), (min-width: 1040px) calc(33.18vw - 62px), (min-width: 780px) calc(50vw - 72px), calc(100vw - 96px)"
-          />
-        </AspectRatio>
+        <a href="">
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              src={image}
+              alt="Bradwell"
+              className="rounded-md object-cover"
+              fill
+              priority
+              sizes="(min-width: 1520px) 289px, (min-width: 1280px) calc(13.64vw + 84px), (min-width: 1040px) calc(33.18vw - 62px), (min-width: 780px) calc(50vw - 72px), calc(100vw - 96px)"
+            />
+          </AspectRatio>
+        </a>
         <CardFooter className="flex flex-col items-start p-2">
           <span className="text-xs">Web app</span>
-          <p className="text-xs">Bradwell Sales & Marketing Corp.</p>
+          <p className="text-xs">{name}.</p>
         </CardFooter>
       </CardContent>
     </Card>
