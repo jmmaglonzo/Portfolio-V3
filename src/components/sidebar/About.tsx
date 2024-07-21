dee89004d820b2911d2ca1a86d1eb0063e2f3cbc
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 23:11:05 +0800

    style: margin adjustment

--- src/components/sidebar/About.tsx
+++ src/components/sidebar/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className="mt-4 hidden w-[30%] flex-col items-start gap-1 md:flex xl:w-full">
+    <div className="hidden w-[30%] flex-col items-start gap-1 px-2 md:flex xl:w-full">
       <span className="text-start font-semibold">About</span>
       <p className="text-xs lg:text-sm">
         A passionate developer who gained coding skills independently through
