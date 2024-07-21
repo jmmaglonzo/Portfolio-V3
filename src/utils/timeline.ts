a7829a01e973aba23be07b12c2b85dfd4b064b97
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 18:48:50 +0800

    feat: add timeline.ts

---
+++ src/utils/timeline.ts
@@ -0,0 +1,20 @@
+export const timeLine = [
+  {
+    title: "Hackathon MVP",
+    date: "July 18, 2024",
+    description:
+      "Recognized as Most Valuable Player for outstanding effort and technical contributions.",
+  },
+  {
+    title: "FrontEnd Developer Intern | Codebility",
+    date: "April 2024 - Present",
+    description:
+      "Frontend developer crafting user interfaces and web experiences at Codebility. Builds responsive applications using modern web technologies, collaborating with design and other developers.",
+  },
+  {
+    title: "Bachelor of Science in Electrical Technology",
+    date: "August 2018 - March 2022",
+    description:
+      "Foundational degree in electrical Technology. Transitioned to web development after one year, applying technical skills to digital realm.",
+  },
+];
