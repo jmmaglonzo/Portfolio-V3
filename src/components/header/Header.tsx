3d71cd47ab433a6e99484a51f7d2282185d60eba
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 14:24:13 +0800

    feat: add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import { DarkMode } from "../theme/Darkmode";
+
+const Header = () => {
+  return (
+    <header className="container flex items-center justify-between">
+      <DarkMode />
+    </header>
+  );
+};
+
+export default Header;
