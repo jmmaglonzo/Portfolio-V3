c1b66addaa482b14be9fc550bb8ac6b831c0ef7c
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 23:10:22 +0800

    refactor: created a seoerate component for skills

--- src/components/resume/Experience.tsx
+++ src/components/resume/Experience.tsx
@@ -1,28 +1,13 @@
-import Image from "next/image";
-import { AspectRatio } from "../ui/aspect-ratio";
+import { Separator } from "../ui/separator";
+import Skills from "./Skills";
 import Timeline from "./Timeline";
-import icons from "@/utils/skills";
 const Experience = () => {
   return (
-    <div>
+    <>
       <Timeline />
-      <span className="my-4 inline-block text-3xl font-bold">Skills</span>
-      <div className="flex flex-wrap items-center gap-3">
-        {icons.map((i) => (
-          <div className="size-12 rounded-sm bg-white p-1" key={i.name}>
-            <AspectRatio ratio={1 / 1} className="size-full">
-              <Image
-                src={i.icon}
-                alt="Icons"
-                fill
-                className="object-contain"
-                sizes="50px"
-              />
-            </AspectRatio>
-          </div>
-        ))}
-      </div>
-    </div>
+      <Separator className="mt-6 h-[2px] rounded-full" />
+      <Skills />
+    </>
   );
 };
 
