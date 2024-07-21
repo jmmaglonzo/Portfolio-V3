37f8543893f79b4744550223f8a714bfb58eb009
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-21 23:12:01 +0800

    style: adjust background color

--- src/components/sidebar/Topbar.tsx
+++ src/components/sidebar/Topbar.tsx
@@ -1,11 +1,11 @@
 import { Separator } from "../ui/separator";
 import About from "./About";
-import Contact from "./Contact";
+import SidebarInfo from "./SidebarInfo";
 import Profile from "./Profile";
 
 const Topbar = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-between rounded-xl bg-slate-900 p-4 md:flex-row md:gap-4 xl:hidden">
+    <div className="flex w-full flex-col items-center justify-between rounded-xl bg-secondary p-4 md:flex-row md:gap-4 xl:hidden">
       <Profile />
       <Separator
         orientation="vertical"
@@ -16,7 +16,7 @@ const Topbar = () => {
         orientation="vertical"
         className="hidden h-36 w-[2px] md:block"
       />
-      <Contact />
+      <SidebarInfo />
     </div>
   );
 };
