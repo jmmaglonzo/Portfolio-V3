f127c624061ed2e28d200398429689091155cef3
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-22 00:05:15 +0800

    feat: add name for skills logo

--- src/components/resume/Skills.tsx
+++ src/components/resume/Skills.tsx
@@ -8,16 +8,24 @@ const Skills = () => {
       <span className="my-4 inline-block text-3xl font-bold">Skills</span>
       <div className="flex flex-wrap items-center gap-3">
         {icons.map((i) => (
-          <div className="size-12 rounded-sm bg-white p-1" key={i.name}>
-            <AspectRatio ratio={1 / 1} className="size-full">
-              <Image
-                src={i.icon}
-                alt="Icons"
-                fill
-                className="object-contain"
-                sizes="50px"
-              />
-            </AspectRatio>
+          <div
+            className="flex items-center gap-4 rounded-[6px] bg-white px-4 py-1.5 text-black"
+            key={i.name}
+          >
+            <div className="size-6">
+              <AspectRatio ratio={1 / 1} className="size-full">
+                <Image
+                  src={i.icon}
+                  alt="Icons"
+                  fill
+                  className="object-contain"
+                  sizes="50px"
+                />
+              </AspectRatio>
+            </div>
+            <span className="hidden text-xs font-semibold lg:inline-block">
+              {i.name}
+            </span>
           </div>
         ))}
       </div>
