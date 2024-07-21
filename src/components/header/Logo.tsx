c1fd546f1d99c5133075cf521ed8dd4fc56d2ac6
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-22 00:04:28 +0800

    feat: created logo component

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,12 @@
+import { CurlyBraces } from "lucide-react";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <CurlyBraces className="size-8" />
+      <span className="text-2xl font-bold">Lonzo</span>
+    </div>
+  );
+};
+
+export default Logo;
