8fe0799cbb077678e63761fef187c0a0a7d9e2a2
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-22 13:24:59 +0800

    style: adjust padding and margin

--- src/components/project/ProjectGrid.tsx
+++ src/components/project/ProjectGrid.tsx
@@ -2,7 +2,7 @@ import ProjectCard from "./ProjectCard";
 import cards from "@/utils/cards";
 const ProjectGrid = () => {
   return (
-    <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-8 px-4 md:grid-cols-2 lg:grid-cols-3">
       {cards.map((c) => (
         <ProjectCard key={c.name} data={c} />
       ))}
