330d0581cdbc4985d16524150dafab68c6190eb8
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-22 13:24:38 +0800

    style:remove bg-secondary class

--- src/components/main/MainContent.tsx
+++ src/components/main/MainContent.tsx
@@ -2,7 +2,7 @@ import Navbar from "../header/Navbar";
 
 const MainContent = () => {
   return (
-    <div className="relative rounded-md bg-secondary px-4 xl:w-[75%] xl:min-w-[75%]">
+    <div className="relative rounded-md xl:w-[75%] xl:min-w-[75%]">
       <Navbar />
     </div>
   );
