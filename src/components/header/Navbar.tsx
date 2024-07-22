4e7112fd8555bd84fe815a6a4ee8a2c22be3ae93
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-22 13:22:58 +0800

    chore: remove the shadcn tabs

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,35 +1,11 @@
-import Contact from "../contact/Contact";
-import ProjectGrid from "../project/ProjectGrid";
-import Experience from "../resume/Experience";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
-
+"use client";
+import tabs from "@/utils/tabCards";
+import { Tabs } from "../ui/tabs";
 const Navbar = () => {
   return (
-    <nav>
-      <Tabs defaultValue="Projects" className="w-full">
-        <TabsList className="absolute right-0 top-0 bg-secondary">
-          <TabsTrigger value="Projects">Projects</TabsTrigger>
-          <TabsTrigger value="Resume">Resume</TabsTrigger>
-          <TabsTrigger value="Contact">Contact</TabsTrigger>
-        </TabsList>
-        <TabsContent value="Projects">
-          <span className="mb-10 inline-block text-3xl font-bold">
-            Projects
-          </span>
-          <ProjectGrid />
-        </TabsContent>
-        <TabsContent value="Resume">
-          <span className="mb-10 inline-block text-3xl font-bold">
-            Experience & Education
-          </span>
-          <Experience />
-        </TabsContent>
-        <TabsContent value="Contact">
-          <span className="mb-10 inline-block text-3xl font-bold">Contact</span>
-          <Contact />
-        </TabsContent>
-      </Tabs>
-    </nav>
+    <div className="relative mx-auto flex h-[42rem] w-full flex-col items-start justify-start">
+      <Tabs tabs={tabs} />
+    </div>
   );
 };
 
