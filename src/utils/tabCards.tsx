685b8c6f576b698bfb7cf217166bb8c2e734d09d
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-22 13:26:02 +0800

    feat: created tabcards component for better readability

---
+++ src/utils/tabCards.tsx
@@ -0,0 +1,38 @@
+import Contact from "@/components/contact/Contact";
+import ProjectGrid from "@/components/project/ProjectGrid";
+import Experience from "@/components/resume/Experience";
+
+const tabs = [
+  {
+    title: "Projects",
+    value: "projects",
+    content: (
+      <div className="no-visible-scrollbar relative h-full overflow-hidden overflow-y-scroll rounded-2xl bg-secondary p-4 font-bold text-white md:p-6">
+        <p className="mb-8 text-base md:text-3xl">Projects</p>
+        <ProjectGrid />
+      </div>
+    ),
+  },
+  {
+    title: "Experience",
+    value: "experience",
+    content: (
+      <div className="no-visible-scrollbar relative h-full overflow-y-scroll rounded-2xl bg-secondary p-4 font-bold text-white md:p-6">
+        <p className="mb-8 text-base md:text-3xl">Experience and Skills </p>
+        <Experience />
+      </div>
+    ),
+  },
+  {
+    title: "Contact",
+    value: "contact",
+    content: (
+      <div className="no-visible-scrollbar relative h-full overflow-hidden overflow-y-scroll rounded-2xl bg-secondary p-4 font-bold text-white md:p-6">
+        <p className="mb-8 text-base md:text-3xl">Playground tab</p>
+        <Contact />
+      </div>
+    ),
+  },
+];
+
+export default tabs;
