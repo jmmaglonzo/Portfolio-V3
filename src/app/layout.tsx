f28f85f7693ba233af98c038d51d088aa74d4fc3
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-24 12:28:11 +0800

    feat: add toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import Header from "@/components/header/Header";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <Toaster richColors position="top-center" />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
