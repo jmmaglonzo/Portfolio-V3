523c5384bf6a142f85bbfa67e2d0c9ccd63c9dfa
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-24 12:29:11 +0800

    style: change border radius

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import About from "./About";
 import SidebarInfo from "./SidebarInfo";
 const Sidebar = () => {
   return (
-    <aside className="hidden h-full max-h-max rounded-md bg-secondary px-4 pb-8 pt-16 xl:sticky xl:top-20 xl:block">
+    <aside className="hidden h-full max-h-max rounded-xl bg-secondary px-4 pb-8 pt-16 xl:sticky xl:top-20 xl:block">
       <Profile />
       <Separator decorative className="mt-4 h-[2px] rounded-sm" />
       <About />
