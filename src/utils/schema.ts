022da6f97a7f73aeb9a6c948a614a728a5526a44
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-24 12:30:08 +0800

    chore: create seperate file for zod validation schema

---
+++ src/utils/schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+export const formSchema = z.object({
+  name: z
+    .string()
+    .min(3, {
+      message: "Name must be at least 3 characters long",
+    })
+    .max(50, {
+      message: "Name must be less than 50 characters long",
+    }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  message: z.string().min(10, {
+    message: "Message must be at least 10 characters long",
+  }),
+});
