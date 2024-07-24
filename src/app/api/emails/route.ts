f1c395e5028610f206ebf306673fb0e5af451849
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-24 14:58:00 +0800

    refactor: add subject

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -3,13 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { name, email, message } = body;
+  const { name, email, message, subject } = body;
 
   try {
     await transporter.sendMail({
-      from: email,
+      from: process.env.EMAIL,
+      replyTo: email,
       to: process.env.EMAIL,
-      subject: "New Contact Form Submission",
+      subject: subject,
       text: `
             Name: ${name}
             Email: ${email}
