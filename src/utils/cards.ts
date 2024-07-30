38846ba260f757db8cbf0865a513b17559209a6b
Lonzo <125773218+jmmaglonzo@users.noreply.github.com>
2024-07-30 18:02:23 +0800

    feat: add stack property which is array of strings

--- src/utils/cards.ts
+++ src/utils/cards.ts
@@ -2,33 +2,47 @@ import bradwell from "/public/bradwell.webp";
 import gymfit from "/public/gymfit.webp";
 import omnifood from "/public/omni-food.webp";
 import portfolio from "/public/portfoliov2.webp";
-import digibite from "/public/digibite-kitchen.webp";
+import digibite from "/public/digibite.png";
 
 const cards = [
   {
     title: "Bradwell Sales & Marketing Corp.",
     image: bradwell,
     link: "https://bradwell-fe.vercel.app/",
+    stack: ["REACT JS", "TAILWIND-CSS"],
   },
-  {
-    title: "Digibite | Digital Menu",
-    image: digibite,
-    link: "https://digibite.vercel.app/",
-  },
+
   {
     title: "GymFit",
     image: gymfit,
     link: "https://gymfit-sage.vercel.app/",
+    stack: ["EXPRESS JS", "REACT JS", "MONGODB"],
   },
   {
     title: "Omnifood-Ai",
     image: omnifood,
     link: "https://omnifood-react.vercel.app/",
+    stack: ["REACT JS", "TAILWIND-CSS"],
+  },
+  {
+    title: "Digibite | Digital Menu",
+    image: digibite,
+    link: "https://digibite.vercel.app/",
+    stack: [
+      "NEXT JS",
+      "TAILWIND-CSS",
+      "MYSQL",
+      "PRISMA",
+      "ZUSTAND",
+      "REACT-QUERY",
+      "EXPRESS JS",
+    ],
   },
   {
     title: "Portfolio v1",
     image: portfolio,
     link: "https://lonzo-react.vercel.app/",
+    stack: ["REACT JS", "TAILWIND-CSS"],
   },
 ];
 
